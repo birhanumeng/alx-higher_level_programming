0bf120d3008f4692689a35eb1315da860fcec61a
birhanumeng <eyerusalemmengiste@gmail.com>
2022-09-26 00:43:37 -0700

    print element at index

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 1-element_at.py
+
+def element_at(my_list, idx):
+    """Print list element at a specific index."""
+    if idx < 0 or idx >= len(my_list):
+        return
+    print("Element at index {:d} is {}".format(idx, my_list[idx]))
