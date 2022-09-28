30cb4b25c79c86e6b498c70a3b4b2719e62c2cd7
birhanumeng <eyerusalemmengiste@gmail.com>
2022-09-28 08:38:05 -0700

    update dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+    """Replace key and value if it exit otherwise add."""
+    a_dictionary[key] = value
+    return a_dictionary
