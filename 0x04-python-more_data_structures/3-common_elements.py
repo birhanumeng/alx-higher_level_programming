3a7ec0a243c4456c21d92dd1430252a150056c64
birhanumeng <eyerusalemmengiste@gmail.com>
2022-09-28 07:55:29 -0700

    print common elemnts in sets

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+    """Print common elements to both set_1 and set_2."""
+    my_set = {}
+    for i in set_1 & set_2:
+        my_set.append(i)
+    return my_set
+
