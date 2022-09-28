4ec4925ca46235ad3edc5d85a34aa0a29b5f7ff6
birhanumeng <eyerusalemmengiste@gmail.com>
2022-09-28 07:32:23 -0700

    add unique elements only

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,6 +3,7 @@
 def uniq_add(my_list=[]):
     """Adds all the unique elements only in list."""
     my_set = set(my_list)
+    sum = 0
     for i in range(len(my_set)):
         sum += my_set[i]
     return sum
