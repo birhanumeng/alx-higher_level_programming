ec8599ef286667825d47fec300c1c50d0a62aacb
birhanumeng <eyerusalemmengiste@gmail.com>
2022-09-28 08:25:52 -0700

    print key and value

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+    """Print key and value in a dictionary."""
+    for key, value in a_dictionary.item():
+        print(key + ": " + value)
