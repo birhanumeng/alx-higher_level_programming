1e83cddbb4eddaf521d2bfea3e77c4c7e99c5169
birhanumeng <eyerusalemmengiste@gmail.com>
2022-09-28 08:32:06 -0700

    print key and value

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,5 +2,5 @@
 
 def print_sorted_dictionary(a_dictionary):
     """Print key and value in a dictionary ordered by key."""
-    for key in a_dictionary.item():
+    for key in sorted(a_dictionary):
         print("{}: {}".format(key, a_dictionary[key]))
