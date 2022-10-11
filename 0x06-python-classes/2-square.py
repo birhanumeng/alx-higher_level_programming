e1c67b212bc0910c7bca07bc49be837e45e1ed75
birhanumeng <eyerusalemmengiste@gmail.com>
2022-10-11 13:48:10 -0700

    initialize

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Square Class"""
+
+
+class Square:
+
+    def __init__(self, size=0):
+        """__init__"""
+
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
