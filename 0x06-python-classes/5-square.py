e83774998e72758977452c97dda80ae374695fec
birhanumeng <eyerusalemmengiste@gmail.com>
2022-10-11 14:03:55 -0700

    class with print

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Square class"""
+
+
+class Square:
+    """A class named Square
+    Attributes:
+    attr1 (size): size of square
+    """
+    def __init__(self, size=0):
+        """
+        Args:
+        size (int): size for __size
+        """
+        self.size = size
+
+    def area(self):
+        """Calculates area"""
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """Gets the size of the object"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the size of the object"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        """Prints in stdout"""
+        if self.__size > 0:
+            for row in range(self.__size):
+                for column in range(self.__size):
+                    print("#", end="")
+                print()
+        else:
+            print()
