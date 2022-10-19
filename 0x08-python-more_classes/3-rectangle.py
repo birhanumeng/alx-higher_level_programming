a2dfa67de9967f1c4807ac1b9cc9b27189c81145
birhanumeng <eyerusalemmengiste@gmail.com>
2022-10-19 09:15:12 -0700

    define rectangle

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""Creates a class for define Rectangle"""
+
+
+class Rectangle:
+    """A class named Rectangle."""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__width * 2) + (self.__height * 2)
+
+    def __str__(self):
+        stringrep = ""
+        if self.__width == 0 or self.__height == 0:
+            return stringrep
+        for row in range(self.__height):
+            for column in range(self.__width):
+                stringrep += "#"
+            if row < self.__height - 1:
+                stringrep += "\n"
+        return stringrep
