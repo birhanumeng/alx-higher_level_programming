0603de8bf91ecd80c78cc66cfccb97799233f5db
birhanumeng <eyerusalemmengiste@gmail.com>
2022-10-25 07:56:37 -0700

    print number of lines

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,7 +5,7 @@
 def number_of_lines(filename=""):
     """Returns number of lines in a text file."""
     count = 0
-    with open(filename, encoding='utf-8') as f:
+    with open(filename) as f:
         for line in f:
             count += 1
         return count
