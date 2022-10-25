2276ea4a13e7cbe284d8b264e051829f680c4074
birhanumeng <eyerusalemmengiste@gmail.com>
2022-10-25 09:27:51 -0700

    describe dictionary

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Define function that describe dictionary."""
+
+
+def class_to_json(obj):
+    """dictionary description with simple data structure."""
+    return obj.__dict__
