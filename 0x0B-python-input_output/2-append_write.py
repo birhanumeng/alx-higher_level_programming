b3ef3754b181f1b702c51e19c02f7d178b6bedaf
birhanumeng <eyerusalemmengiste@gmail.com>
2022-10-25 11:05:01 -0700

    append string

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,13 +2,7 @@
 """Define function tha append string to a file."""
 
 
-def read_lines(filename="", nb_lines=0):
-    """prints number of lines in a text file."""
-    line_count = 0
-    with open(filename, 'r', encoding='utf-8') as f:
-        if nb_lines <= 0:
-            print(f.read(), end="")
-        for line in f:
-            if line_count < nb_lines:
-                print(line, end="")
-                line_count += 1
+def append_write(filename="", text=""):
+    """append string to a file."""
+    with open(filename, 'a', encoding='utf-8') as file:
+        return file.write(text)
