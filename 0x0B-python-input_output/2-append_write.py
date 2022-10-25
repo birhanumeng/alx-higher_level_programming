9f00ea8a1e34ed148c22d13b852766d9fc85bb64
birhanumeng <eyerusalemmengiste@gmail.com>
2022-10-25 08:09:53 -0700

    print number of string added

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -4,21 +4,11 @@
 
 def read_lines(filename="", nb_lines=0):
     """prints number of lines in a text file."""
-    with open(filename, encoding="utf-8") as f:
+    line_count = 0
+    with open(filename, 'r', encoding='utf-8') as f:
         if nb_lines <= 0:
             print(f.read(), end="")
-            return
-
-        lines = 0
         for line in f:
-            lines += 1
-        f.seek(0)
-        if nb_lines >= lines:
-            print(f.read(), end="")
-            return
-
-        else:
-            n = 0
-            while n < nb_lines:
-                print(f.readline(), end="")
-                n += 1
+            if line_count < nb_lines:
+                print(line, end="")
+                line_count += 1
