90f350e6dacad220ca62bfb26c87adc89d8eed08
birhanumeng <eyerusalemmengiste@gmail.com>
2022-11-30 18:25:45 -0800

    import the database dump

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,7 @@
+-- Import the database hbtn_0d_tvshows_rate dump
+SELECT `title`, SUM(`rate`) AS `rating`
+  FROM `tv_shows` AS tv
+       INNER JOIN `tv_show_ratings` AS tv_r
+       ON tv.`id` = tv.r.`show_id`
+ GROUP BY `title`
+ ORDER BY `rating` DESC;
