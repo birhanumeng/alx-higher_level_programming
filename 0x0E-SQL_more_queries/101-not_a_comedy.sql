fa7dcf17dd9066deadfdaa640a4feb319771b81b
birhanumeng <eyerusalemmengiste@gmail.com>
2022-11-30 18:42:33 -0800

    import the databse dump

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,18 +1,18 @@
 -- Import the database dump from hbtn_0d_tvshows
 SELECT DISTINCT `title`
-  FROM `tv_shows` AS tv
-       LEFT JOIN `tv_show_genres` AS t_sg
-       ON t_sg.`show_id` = tv.`id`
+  FROM `tv_shows` AS t
+       LEFT JOIN `tv_show_genres` AS s
+       ON s.`show_id` = t.`id`
 
-       LEFT JOIN `tv_genres` AS t_g
-       ON t_g.`id` = t_sg.`genre_id`
+       LEFT JOIN `tv_genres` AS g
+       ON g.`id` = s.`genre_id`
        WHERE tv.`title` NOT IN
              (SELECT `title`
-                FROM `tv_shows` AS tv
-	             INNER JOIN `tv_show_genres` AS t_sg
-		     ON t_sg.`show_id` = tv.`id`
+                FROM `tv_shows` AS t
+	             INNER JOIN `tv_show_genres` AS s
+		     ON s.`show_id` = t.`id`
 
-		     INNER JOIN `tv_genres` AS t_g
-		     ON t_g.`id` = t_sg.`genre_id`
+		     INNER JOIN `tv_genres` AS g
+		     ON g.`id` = s.`genre_id`
 		     WHERE g.`name` = "Comedy")
  ORDER BY `title`;
