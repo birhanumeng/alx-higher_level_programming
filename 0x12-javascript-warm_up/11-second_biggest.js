4206ebe4adb2baeef5d803a3c9eb1dbaaf3b97af
birhanumeng <eyerusalemmengiste@gmail.com>
2022-12-19 12:47:43 -0800

    print second max

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,15 +3,17 @@ const arrLen = process.argv.length;
 if (arrLen < 4) {
   console.log(0);
 } else {
-  let i = 3;
-  let max = process.argv[2];
-  let secondMax = max;
+  let i = 2;
+  let max = 0;
+  let secondMax = 0;
   let tmp;
   while (i < arrLen) {
     tmp = max;
     if (process.argv[i] > max) {
       max = process.argv[i];
       secondMax = tmp;
+    } else if (process.argv[i] > secondMax) {
+      secondMax = process.argv[i];
     }
     i++;
   }
