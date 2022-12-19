eb6753c84d7472d797891e372967fa2b963bd2c5
birhanumeng <eyerusalemmengiste@gmail.com>
2022-12-19 05:47:06 -0800

    check number of arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const arg_v = process.argv.length;
 if (arg_v === 2) {
-	console.log('No argument');
+  console.log('No argument');
 } else if (arg_v === 3) {
-	console.log('Argument found');
+  console.log('Argument found');
 } else {
-	console.log('Arguments found');
+  console.log('Arguments found');
 }
