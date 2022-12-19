1239142c37b50931741442ea1e13b4078bde650c
birhanumeng <eyerusalemmengiste@gmail.com>
2022-12-19 05:41:21 -0800

    check number of arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-import { argv } from 'node:process';
-if (argv.length === 2) console.log('No argument');
-if (argv.length === 3) console.log('Argument found');
-if (argv.length > 3) console.log('Arguments found');
+const arg_v = process.argv.length;
+if (arg_v === 2) console.log('No argument');
+if (arg_v === 3) console.log('Argument found');
+if (arg_v > 3) console.log('Arguments found');
