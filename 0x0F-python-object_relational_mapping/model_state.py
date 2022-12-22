8efac78be54bcad3a7e88e8be49270beb2dc90e3
birhanumeng <eyerusalemmengiste@gmail.com>
2022-12-22 06:11:24 -0800

    creste class definition

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 """Class definition of a State and an instance Base"""
-from sys import argv
-from model_state import Base, State
 
-from sqlalchemy import (create_engine)
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """class that inherits from Base"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
