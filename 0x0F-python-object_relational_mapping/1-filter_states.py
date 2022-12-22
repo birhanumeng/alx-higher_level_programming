e5824ecee118fbc41c5176aca7dd1ccabfb8e344
birhanumeng <eyerusalemmengiste@gmail.com>
2022-12-22 00:31:32 -0800

    select name starting with N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,7 +4,7 @@
 from sys import argv
 import MySQLdb
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3], charset="utf8")
     cur = conn.cursor()
