e03f6ee194598fcaa539599303ab4d8563e720d7
birhanumeng <eyerusalemmengiste@gmail.com>
2022-12-22 00:14:05 -0800

    select name starting with N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,11 +4,11 @@
 import argv from sys
 import MySQLdb
 
-if __name__ == 'main':
-    conn = MySQLdb.connect(host = "localhost", port = 3306, user = argv[1],
-                           passwd = argv[2], db = argv[3], charset = "charset")
+if __name__ == '__main__':
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
     cur = conn.cursor()
-    cur.excute("SELECT name FROM states WHERE name LIKE 'N%' ORDER BY states.id")
+    cur.execute("SELECT name FROM states WHERE name LIKE 'N%' ORDER BY states.id")
     query_rows = cur.fetchall()
     for(row in query_rows):
         print(row)
