868ab74e5dee68c5f8bc8bd8d9c62fdf6d355aa4
birhanumeng <berhanumengstie@gmail.com>
2023-01-17 12:11:44 -0800

    interview

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,11 +5,13 @@ import sys
 
 if __name__ == "__main__":
 
-    url = "https://developer.github.com/{}/{}/repos/commits".format(
+    url = "https://api.github.com/repos/{}/{}/commits".format(
         sys.argv[2], sys.argv[1])
     req = requests.get(url)
-
-    for i in range(10):
+    try:
+        for i in range(10):
             print("{}: {}".format(req.json()[i].get("sha"),
                                   req.json()[i].get("commit").
                                   get("author").get("name")))
+    except IndexError:
+        pass
