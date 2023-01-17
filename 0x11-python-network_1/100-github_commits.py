09c1bcd6166dad24256741065bd85b8728668611
birhanumeng <berhanumengstie@gmail.com>
2023-01-17 12:05:37 -0800

    interview

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,7 +5,7 @@ import sys
 
 if __name__ == "__main__":
 
-    url = "https://developer.github.com/repos/{}/{}/commits".format(
+    url = "https://developer.github.com/{}/{}/repos/commits".format(
         sys.argv[2], sys.argv[1])
     req = requests.get(url)
 
