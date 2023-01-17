f422365cec89712bc64c384577e2088a98081a51
birhanumeng <berhanumengstie@gmail.com>
2023-01-17 08:15:51 -0800

    read url body

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status."""
+"""fetches https://intranet.hbtn.io/status."""
 import urllib.request
 
 
 if __name__ == "__main__":
-    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as response:
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
         body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
