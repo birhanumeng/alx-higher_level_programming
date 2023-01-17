45f1286875986a0efef4d6959ffb8c4e22ef9f68
birhanumeng <berhanumengstie@gmail.com>
2023-01-17 08:31:29 -0800

    take url argument and read ut

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" takes in a URL, sends a request to the URL and 
+displays the value of the X-Request-Id
+"""
+import sys
+import urllib.request
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        print(dict(response.headers).get("X-Request-Id"))
