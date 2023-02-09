3f72d0a4db3408272f9fef9b8e203283b088ef01
= <=>
2023-02-09 12:43:25 +0000

    parse title in movie

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,7 @@
 
 const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/';
-const id = process.arg[2];
+const id = process.argv[2];
 request(url + id, (err, res, body) => {
   if (err) {
     console.log(err);
