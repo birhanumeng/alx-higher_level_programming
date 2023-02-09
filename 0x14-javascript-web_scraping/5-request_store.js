48b43b95b7f9eba2d7b3f1bc0b4f9d7314b5b7a5
= <=>
2023-02-09 13:13:56 +0000

    write content of web page

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -9,7 +9,7 @@ request.get(url, (err, res, body) => {
   if (err) {
     console.log(err);
   }
-  fs.writeFile(file_path, body, 'utf8', err => {
+  fs.writeFile(file_path, body, 'utf8', (err) => {
     if (err) {
       console.log(error);
     }
