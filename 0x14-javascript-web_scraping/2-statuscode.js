a6bed07f80fe0536d2caa9fd2cfeb057bc57801e
= <=>
2023-02-09 12:30:40 +0000

    print status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-const url = process.argv[2];
 const request = require('request');
+const url = process.argv[2];
 request(url, (error, response) => {
   if (error) {
     console.log(error);
