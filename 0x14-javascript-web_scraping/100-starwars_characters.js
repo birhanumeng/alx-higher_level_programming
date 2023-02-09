85e86542194173dba3cec00dc8c30a9e1ebdc185
= <=>
2023-02-09 13:39:25 +0000

    Who was playing in this movie

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -6,8 +6,8 @@ const url = 'https://swapi.co/api/films/' + process.argv[2];
 request(url, (err, res, body) => {
   if (!err) {
     const chars = JSON.parse(body).characters;
-    chars.forEach((ch) => {
-      request(ch, (err, res, body) => {
+    chars.forEach((character) => {
+      request(character, (err, res, body) => {
         if (!err) {
           console.log(JSON.parse(body).name);
         }
