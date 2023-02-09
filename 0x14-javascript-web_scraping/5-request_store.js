0570617723dc967ae206702e985dd1ae7b8df322
= <=>
2023-02-09 13:08:51 +0000

    write content of web page

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,14 +1,15 @@
 #!/usr/bin/node
 
-const requests = require('request');
+const request = require('request');
 const fs = require('fs');
 const url = process.argv[2];
 const file_path = process.argv[3];
-requests.get(url, (err, res, body) {
+
+request.get(url, (err, res, body) {
   if (err) {
     console.log(err);
   }
-  fs.writeFile(file_path, body, 'utf8', err => {
+  fs.writeFile(file_path, body, 'utf8', (err) => {
     if (err) {
       console.log(error);
     }
