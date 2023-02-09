a15c0e0d18208f949e7ed2deb4f9a512081d5d6d
= <=>
2023-02-09 11:41:16 +0000

    read file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs');
 const file = process.argv[2];
-fs.readFile(file, 'utf8', function read(data, err) {
+fs.readFile(file, 'utf8', function read (data, err) {
   if (err) {
     console.log(err);
     return;
