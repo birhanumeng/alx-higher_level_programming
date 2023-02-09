47b54ff4d7d04649073ba61adb2481a781669954
= <=>
2023-02-09 13:36:00 +0000

    Who was playing in this movie

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = 'https://swapi.co/api/films/' + process.argv[2];
+
+request(url, (err, res, body) => {
+  if (!err) {
+    const chars = JSON.parse(body).characters;
+    chars.forEach((ch) => {
+      request(ch, (err, res, body) {
+        if (!err) {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    });
+  }
+});
