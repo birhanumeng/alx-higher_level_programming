ca6b6619ce6607126fb0866d5bccd9ac20543519
= <=>
2023-02-20 20:00:45 +0000

    add class red to the header

---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,6 @@
+// adds the class red to the <header> element when the user clicks on
+// the tag DIV#red_header using JQuery API
+
+$('DIV#red_header').click(function () {
+    $('header').addClass('red');
+});
